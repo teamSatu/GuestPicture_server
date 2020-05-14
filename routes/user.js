6bea15e0cddcdb867ebefeaf714c94c264de602b
jesicaahr <jesicaprilia18@gmail.com>
2020-05-14 15:16:11 +0700

    edit delete controller

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ const UserController = require('../controllers/userController')
 router.post('/login', UserController.login)
 router.get('/showPlayer', UserController.showPlayer)
 router.patch('/:id', UserController.addScore)
-router.delete('/:idRoom', UserController.deletePlayer)
+router.delete('/', UserController.deletePlayer)
 
 module.exports = router
